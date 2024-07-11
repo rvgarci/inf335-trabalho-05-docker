94b285282a6bf5d5b49e060be532a46ddd5e57b7
Rafael Garcia <rvgarci@gmail.com>
2024-07-10 22:21:09 -0300

    update class

---
+++ src/main/java/br/unicamp/ic/inf335/OlaUnicamp.java
@@ -0,0 +1,12 @@
+package br.unicamp.ic.inf335;
+
+
+public class OlaUnicamp {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		System.out.println("Olá, Unicamp!");
+	
+	}
+}
---
+++ src/test/java/br/unicamp/ic/inf335/OlaUnicampTest.java
@@ -0,0 +1,11 @@
+package br.unicamp.ic.inf335;
+
+import junit.framework.TestCase;
+
+public class OlaUnicampTest extends TestCase {
+
+	public void testMain() {
+		assertTrue("Pass", true);;
+	}
+
+}
